8efa4f8617882abcbecd29c039752b2400839447
Matt Morden <Mattymorden@gmail.com>
2020-06-22 23:00:45 -0400

    Init commit. Project shell

---
+++ MordyGolf.Api/MordyGolf.Api.UnitTests/UserControllerTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace MordyGolf.Api.UnitTests
+{
+    public class UserControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ MordyGolf.Api/MordyGolf.DataContracts/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MordyGolf.DataContracts
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+}
